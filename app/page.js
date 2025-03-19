8f6536b89308109d7b112c8ce408cfbb24528461
Omar Wasim <omar.wasim@valpo.edu>
2025-03-19 16:12:21 -0500

    fix

--- app/page.js
+++ app/page.js
@@ -84,7 +84,7 @@ export default function Home() {
                   <div key={index} style={{ border: "1px solid #ccc", padding: "10px", marginTop: "10px" }}>
                     <h3>Scan #{index + 1}</h3>
                     <p><strong>Card Name:</strong> {scan.ximilarData._objects[0]["_identification"]["best_match"]["full_name"]}</p>
-                    <p><strong>Pricing:</strong> ${scan.ximilarData._objects[0]["_identification"]["best_match"]?.pricing?.list[0]?.price ?? (scan.productData[0]["pricing-price"] / 100).toFixed(2)}</p>
+                    <p><strong>Pricing:</strong> ${scan.ximilarData._objects[0]["_identification"]["best_match"]?.pricing ? scan.ximilarData._objects[0]["_identification"]["best_match"]?.pricing?.list[0]?.price : (scan.productData[0]["pricing-price"] / 100).toFixed(2)}</p>
                   </div>
               ))}
             </div>
